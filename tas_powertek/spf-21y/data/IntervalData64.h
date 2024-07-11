459019292554e47035bc981153a6ee0b19665fd6
Gaurav Mogre <gaurav.mogre@gmail.com>
2024-07-11 15:58:21 -0700

    IntervalData64
    Created IntervalData64

--- tas_powertek/spf-21y/data/IntervalData64.h
+++ tas_powertek/spf-21y/data/IntervalData64.h
@@ -16,19 +16,16 @@ namespace tas_powertek::spf21y {
 struct IntervalData64 {
   TimeData time;
   hertz supplyFrequency;
-  volt phase1ToNeutralVoltage_RMS;
-  volt phase2ToNeutralVoltage_RMS;
-  volt phase3ToNeutralVoltage_RMS;
+  volt averagePhaseToNeutralVolage_RMS;
   volt averagePhaseToNeutralVoltage_Fundamental;
   volt averagePhaseToNeutralVolage_THD;
   volt averagePhaseToPhaseVoltage_RMS;
   volt averagePhaseToPhaseVoltage_Fundamental;
-  amp phase1Current_RMS;
-  amp phase2Current_RMS;
-  amp phase3Current_RMS;
-  amp neutralCurrent_RMS;
-  amp averagePhaseCurrent_Fundamental;
+  amp averagePhaseCurrent_RMS;
   amp averagePhaseCurrent_THD;
+  watt overall3PhActivePower_Fundamental;
+  var overall3PhReactivePower_Fundamental;
+  dpf powerFactor_Fundamental_OverallThreePhase;
   amp capacitorPhase1Current_RMS;
   amp capacitorPhase2Current_RMS;
   amp capacitorPhase3Current_RMS;
@@ -36,26 +33,7 @@ struct IntervalData64 {
   amp capacitorPhase1Current_Fundamental;
   amp capacitorPhase2Current_Fundamental;
   amp capacitorPhase3Current_Fundamental;
-  amp capacitorPhase1Current_THD;
-  amp capacitorPhase2Current_THD;
-  amp capacitorPhase3Current_THD;
-  percent capacitorPhase1Current_THDPercent;
-  percent capacitorPhase2Current_THDPercent;
-  percent capacitorPhase3Current_THDPercent;
-  pf powerFactor_OverallThreePhase;
-  dpf powerFactor_Fundamental_OverallThreePhase;
-  va overall3PhApparentPower_Combined;
-  watt overall3PhActivePower_Fundamental;
-  var overall3PhReactivePower_Fundamental;
-  va overall3PhApparentPower_Fundamental;
-  va overall3PhDistortionPower_IEC_IEEE;
-  watt phase1ActivePower_Fundamental;
-  watt phase2ActivePower_Fundamental;
-  watt phase3ActivePower_Fundamental;
-  var phase1ReactivePower_Fundamental;
-  var phase2ReactivePower_Fundamental;
-  var phase3ReactivePower_Fundamental;
-  var capacitorOverallInjectedReactivePower_Fundamental;
+  amp capacitorAverageCurrent_THD;
   FaultDataLow faultData;
   AuxFnData auxFnData;
 
@@ -63,6 +41,6 @@ struct IntervalData64 {
   static IntervalData64 fromByteStream(std::string_view data);
 } FOLLY_PACK_ATTR;
 
-static_assert(sizeof(IntervalData64) == 174);
+static_assert(sizeof(IntervalData64) == 95);
 static_assert(std::is_standard_layout_v<IntervalData64>);
 }  // namespace tas_powertek::spf21y
