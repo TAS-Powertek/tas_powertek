24ed61343ced9843d7c7c1f667467d4205b43676
Gaurav Mogre <gaurav.mogre@gmail.com>
2024-07-29 21:56:20 -0700

    Update cloudshell_setup.sh

--- aws/cloudshell_setup.sh
+++ aws/cloudshell_setup.sh
@@ -5,11 +5,13 @@ yes | sudo yum install perl
 yes | sudo yum install libcurl-devel
 yes | sudo yum install cmake
 yes | sudo yum install ninja-build
+yes | sudo yum install git
 
+CUR_USER=`whoami`
 # Install vcpkg
 pushd .
 sudo mkdir /home/vcpkg
-sudo chown cloudshell-user /home/vcpkg
+sudo chown ${CUR_USER} /home/vcpkg
 cd /home/vcpkg
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg && ./bootstrap-vcpkg.sh
